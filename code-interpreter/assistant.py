0b38108aa6978f8e8f0e4b704d1878dbf9b9853c
Chris Collins <chris@tipodean.com>
2024-04-23 08:58:44 -0700

    Update assistant.py

--- code-interpreter/assistant.py
+++ code-interpreter/assistant.py
@@ -6,7 +6,7 @@ import time
 vector_stores = {
     "Q&A Docs": "vs_TI5Xe5OnNrnM0T56iZhsxuI2",
     "API Docs": "vs_zLv5Fyne1KfIaaT3L3zyQO6z",
-    # Add more stores as needed
+    "Everything Sent" : "vs_hOHJ5iHh123rmpF9HqWbuh3z",
 }
 
 
