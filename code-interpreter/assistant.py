cd3020fa087b3242017b4369db0f154a28ead6e0
Chris Collins <chris@tipodean.com>
2024-04-22 17:25:50 -0700

    Update assistant.py

--- code-interpreter/assistant.py
+++ code-interpreter/assistant.py
@@ -66,3 +66,5 @@ if prompt := st.chat_input():
     # st.write(messages.data[::-1])
     for line in messages.data[::-1]:
         st.chat_message(line.role, avatar=avatar[line.role]).write(line.content[0].text['value'])
+      
+
